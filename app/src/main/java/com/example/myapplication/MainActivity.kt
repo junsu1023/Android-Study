f20fe9ecac4b96b1a32817204497fbf3c3a408c7
junsu1023 <12jangdokg55@gmail.com>
2022-08-15 22:16:24 +0900

    MainActivity에서 Fragment 호출

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentA: Fragment = AFragment()
+        val fragmentB: Fragment = BFragment()
+
+        val aFragmentButton: Button = findViewById(R.id.fragmentA_button)
+        val bFragmentBButton: Button = findViewById(R.id.fragmentB_button)
+
+        aFragmentButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragmentA).commit()
+        }
+
+        bFragmentBButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragmentB).commit()
+        }
     }
 }
