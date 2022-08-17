41b6d0dceb15d4309627d73bfa4133dcb71bf674
junsu1023 <12jangdokg55@gmail.com>
2022-08-17 19:16:21 +0900

    Alert Dialog 기능 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var numberTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count")
+        }
+
         setContentView(R.layout.activity_main)
+
+
+
+        numberTextView = findViewById(R.id.number_text)
+        val countButton: Button = findViewById(R.id.count_button)
+        val dialogButton: Button = findViewById(R.id.dialog_button)
+
+        countButton.setOnClickListener {
+            count++
+            numberTextView.text = count.toString()
+        }
+
+        dialogButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.apply {
+                setTitle(R.string.dialog).setMessage(R.string.select_option)
+                    .setPositiveButton(R.string.initialization) { _, _ ->
+                        count = 0
+                        numberTextView.text = count.toString() }
+                    .setNeutralButton(R.string.toast) { _, _ ->
+                        Toast.makeText(context, R.string.notice, Toast.LENGTH_SHORT).show() }
+                    .setNegativeButton(R.string.none) { dialog, _ ->
+                        dialog.dismiss() }
+            }.show()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("count", count)
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        numberTextView.text = savedInstanceState.getInt("count").toString()
     }
 }
+
