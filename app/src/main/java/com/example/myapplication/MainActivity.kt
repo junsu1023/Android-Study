352ef44a40f20a6ae28b4bdd2b2b315182dad969
junsu1023 <12jangdokg55@gmail.com>
2022-08-21 22:29:12 +0900

    example Bottom Navigation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main_view, HomeFragment()).commit()
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+
+        bottomNavigation.run {
+            setOnItemSelectedListener { item ->
+                when(item.itemId) {
+                    R.id.bottom_home -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, HomeFragment()).commit()
+                        true
+                    }
+                    R.id.bottom_user -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, UserFragment()).commit()
+                        true
+                    }
+                    R.id.bottom_setting -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, SettingFragment()).commit()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
     }
 }
