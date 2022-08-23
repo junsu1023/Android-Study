712cb2e62ddd41b843133df4812ae81639e0d80e
junsu1023 <12jangdokg55@gmail.com>
2022-08-23 18:54:05 +0900

    ViewPager 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        val viewPagerAdapter = FragmentAdapter(this)
+
+        viewPagerAdapter.addColor()
+
+        viewPager.adapter = viewPagerAdapter
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
     }
 }
