5a8d9f35f09ee319fd35e55ff2598229b4fdef04
junsu1023 <12jangdokg55@gmail.com>
2022-08-24 12:00:16 +0900

    TabLayout 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val homeFragment: Fragment = HomeFragment()
+        val userFragment: Fragment = UserFragment()
+        val settingFragment: Fragment = SettingFragment()
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_view, homeFragment).commit()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when(tab.position) {
+                    0 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, homeFragment).commit()
+                    }
+                    1 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, userFragment).commit()
+                    }
+                    2 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, settingFragment).commit()
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
