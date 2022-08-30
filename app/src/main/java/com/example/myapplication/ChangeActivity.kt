6d31b1019f88b68fe9beceec15f3de9a41fcc5cb
junsu1023 <12jangdokg55@gmail.com>
2022-08-30 18:00:52 +0900

    notification 클릭시 띄울 창 구현

--- app/src/main/java/com/example/myapplication/ChangeActivity.kt
+++ app/src/main/java/com/example/myapplication/ChangeActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
 
 class ChangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change)
+
+        val count = intent.getIntExtra("count", 0)
+        val receiveNumTextView: TextView = findViewById(R.id.receive_number_text_view)
+
+        val randomNuｍ = Random.nextInt(count)
+        receiveNumTextView.text = randomNuｍ.toString()
     }
 }
