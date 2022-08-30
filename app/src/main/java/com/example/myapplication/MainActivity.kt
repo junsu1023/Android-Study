8ee1a1ae8235057ca01ac4deeb616085f56ad575
junsu1023 <12jangdokg55@gmail.com>
2022-08-30 18:01:00 +0900

    notification 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.myapplication
-
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+private const val CHANNEL_ID = "my_channel"
+private const val NOTIFICATION_ID = 1023
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberView: TextView = findViewById(R.id.number_view)
+        val countButton: Button = findViewById(R.id.count_button)
+        val randomButton: Button = findViewById(R.id.random_button)
+
+        countButton.setOnClickListener {
+            count++
+            numberView.text = count.toString()
+        }
+
+        createNotification()
+        randomButton.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun createNotification() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun displayNotification() {
+        val intent = Intent(this, ChangeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra("count", count)
+        }
+
+        val pendingIntent: PendingIntent =
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+            else {
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_info)
+            .setContentTitle(getString(R.string.notification_name))
+            .setContentText(getString(R.string.notification_text))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(NOTIFICATION_ID, builder.build())
+        }
     }
 }
